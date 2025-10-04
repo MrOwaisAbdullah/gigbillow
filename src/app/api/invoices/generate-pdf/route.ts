67f0b0bc73a491c179b170485caed8b4a8234380
Owais Abdullah <mrowaisabdullah@gmail.com>
2025-10-04 10:55:52 +0000

    Type 'string | null' is not assignable to type 'string'.   Type 'null' i

--- src/app/api/invoices/generate-pdf/route.ts
+++ src/app/api/invoices/generate-pdf/route.ts
@@ -61,7 +61,7 @@ export async function POST(req: NextRequest) {
         email: user.email || '',
         displayName: user.displayName || '',
         photoURL: user.photoURL || '',
-        phoneNumber: user.phoneNumber || null, // phoneNumber can be null on UserInfo type
+        phoneNumber: user.phoneNumber || '',
         providerId: user.providerData?.[0]?.providerId || '',
         toJSON: () => ({...user})
     };
