a25a8869b11a896aacffc1a1979dfd25c21d5e74
Owais Abdullah <mrowaisabdullah@gmail.com>
2025-10-05 12:21:54 +0000

    increase a little bit more

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -37,7 +37,7 @@ function AppContent({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <AppHeader />
-        <main className="grid flex-1 items-start gap-4 p-4 pb-24 sm:px-6 sm:py-0 md:gap-8 md:pb-16">
+        <main className="grid flex-1 items-start gap-4 p-4 pb-32 sm:px-6 sm:py-0 md:gap-8 md:pb-24">
           {children}
         </main>
       </div>
